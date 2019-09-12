f14b700f9cdfbddc21b8f8cf74a218c7fcb953b1
Levi Miller <paullevimiller@gmail.com>
2019-09-12 12:36:17 -0700

    some progress

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun(param="roller-skate") {
+    return `This Saturday, I want to ${param}!`;
+};
+
+const monday = function(param="go to the office") {
+    return `This Monday, I will ${param}.`
+};
